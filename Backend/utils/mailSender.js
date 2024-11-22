50c446f3bc2578a1171ee367755c9c5b29286b92
vjkumar77 <vijaykumar9024180@gmail.com>
2024-11-22 21:07:53 +0530

    define a mail sender function

---
+++ Backend/utils/mailSender.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, otp) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: 'LinkUp Platform',
+                to:`${email}`,
+                subject: 'Your OTP',
+                text: `Your OTP is: ${otp}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+}
+
+
+module.exports = mailSender;
