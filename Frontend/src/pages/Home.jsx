94d76ab4ea6d60c00fdfd4f7302a36b5c19fc52f
Virendra Kumar <vk98982424@gmail.com>
2024-11-25 21:24:10 +0530

    Middle-Section or Post-View section is made scrollable

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -6,15 +6,20 @@ import UserSuggested from "../components/home/UserSuggested";
 
 export default function Home() {
   return (
-    <div className="w-[90%] lg:pb-5 pb-3 lg:pt-8 pt-4   border-4 border-emerald-600 mx-auto flex  lg:flex-row flex-col justify-between gap-8 min-h-screen">
-      <div className="lg:w-[30%] w-full">
+    <div className="w-[90%] lg:pb-5 pb-3 lg:pt-4 pt-4 border-4 border-emerald-600 mx-auto flex lg:flex-row flex-col justify-between gap-8 min-h-screen">
+      {/* RIGHT-section (Sticky on Large Screens) */}
+      <div className="lg:w-[30%] w-full lg:sticky lg:top-8 self-start border-2 border-red-500 h-fit">
         <UserDetails />
       </div>
-      <div className="flex flex-col gap-y-4 lg:w-[40%] w-full">
+
+      {/* Middle Section (Scrollable) */}
+      <div className="flex flex-col gap-y-4 lg:w-[40%] w-full lg:overflow-y-auto lg:max-h-[calc(100vh-64px)]  lg:scrollbar-hide border-2 border-yellow-400">
         <CreatePost />
         <ShowPost />
       </div>
-      <div className="lg:w-[30%] w-full">
+
+      {/* LEFT-section (Sticky on Large Screens) */}
+      <div className="lg:w-[30%] w-full lg:sticky lg:top-8 self-start border-2 border-red-500 h-fit">
         <UserSuggested />
       </div>
     </div>
