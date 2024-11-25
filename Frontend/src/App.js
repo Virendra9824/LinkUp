0dd9250c844778ed45b620d843b24c8d07ad5a35
vjkumar77 <vijaykumar9024180@gmail.com>
2024-11-25 22:04:07 +0530

    define route for authentication

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,11 +6,13 @@ import Chat from "./pages/Chat";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 import UpdateProfile from "./components/forms/UpdateProfile";
-import SignUpForm from "./pages/SignUpForm";
 import Login from "./pages/Login";
 import DeleteAccount from "./components/forms/DeleteAccount";
 import UpdatePassword from "./components/forms/UpdatePassword";
 import Notification from "./pages/Notification";
+import FirstView from "./pages/firstView";
+import RegisterUpdateForm from "./components/forms/RegisterUpdateForm";
+import SignUpForm from "./components/forms/Signup";
 
 export default function App() {
   return (
@@ -19,12 +21,16 @@ export default function App() {
       <div className="pt-20">
         <Routes>
           <Route path={"/"} element={<Home />} />
+          <Route path={"/new-user"} element={<FirstView />} />
+
+          
           <Route path={"/chat"} element={<Chat />} />
           <Route path={"/notification"} element={<Notification />} />
-          <Route path={"/auth/update-profile"} element={<UpdateProfile />} />
+          <Route path={"/update-profile"} element={<UpdateProfile />} />
+          <Route path={"/auth/signup"} element={<SignUpForm />} />
           <Route path={"/auth/update-password"} element={<UpdatePassword />} />
           <Route path={"/auth/delete-account"} element={<DeleteAccount />} />
-          <Route path={"/auth/signup"} element={<SignUpForm />} />
+          <Route path={"/auth/register"} element={< RegisterUpdateForm/>} />
           <Route path={"/auth/login"} element={<Login />} />
         </Routes>
       </div>
