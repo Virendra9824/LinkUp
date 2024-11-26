ed7cafcb2dd19142157f92a1e345d38830c8f942
Virendra Kumar <vk98982424@gmail.com>
2024-11-26 15:15:11 +0530

    Signup form is connected with backend and send OTP functionality is added.

--- Frontend/src/components/forms/RegisterUpdateForm.jsx
+++ Frontend/src/components/forms/RegisterUpdateForm.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { LuEye } from "react-icons/lu";
 import { LuEyeOff } from "react-icons/lu";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import ImageUploader from "../common/ImageUploader";
+import toast from "react-hot-toast";
+import { sendOTP } from "../../apis/authApi";
+import { signUp } from "../../apis/authApi";
 
 export default function RegisterUpdateForm(props) {
   const { isUpdateForm } = props;
@@ -11,18 +14,23 @@ export default function RegisterUpdateForm(props) {
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
-    location: "",
-    occupation: "",
     email: "",
     password: "",
     confirmPassword: "",
     otp: "",
+    gender: "male",
+    role: "student",
   });
 
   // State for form errors
   const [formErrors, setFormErrors] = useState({});
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [sendOtpLoading, setSendOtpLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [sendOtpBtnMessage, setSendOtpBtnMessage] = useState("Send OTP");
+  const navigate = useNavigate();
+  const [profilePic, setProfilePic] = useState(null);
 
   // Form validation function
   const validateForm = () => {
@@ -34,11 +42,11 @@ export default function RegisterUpdateForm(props) {
     if (!formData.lastName.trim()) {
       errors.lastName = "Last name is required";
     }
-    if (!formData.location.trim()) {
-      errors.location = "Location is required";
+    if (!formData.gender) {
+      errors.gender = "Gender is required";
     }
-    if (!formData.occupation.trim()) {
-      errors.occupation = "Occupation is required";
+    if (!formData.role) {
+      errors.role = "Role is required";
     }
     if (!formData.email) {
       errors.email = "Email is required";
@@ -62,11 +70,37 @@ export default function RegisterUpdateForm(props) {
   };
 
   // Handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (validateForm()) {
-      console.log("Form data submitted:", formData);
-      // Submit form or make API call here
+      if (profilePic) formData.append("profilePic", profilePic);
+
+      // console.log("Form data submitted is:", formData);
+
+      const toastId = toast.loading("Loading...");
+      try {
+        setLoading(true);
+        const response = await signUp(formData);
+        console.log("Response of Signup form: ", response);
+        toast.success("Signup successfully");
+        navigate("/auth/login");
+      } catch (error) {
+        toast.error("Invalid OTP.");
+        console.log("Error while signup: ", error);
+      } finally {
+        toast.dismiss(toastId);
+        setLoading(false);
+        // setFormData({
+        //   firstName: "",
+        //   lastName: "",
+        //   email: "",
+        //   password: "",
+        //   confirmPassword: "",
+        //   otp: "",
+        //   gender: "male",
+        //   role: "student",
+        // });
+      }
     }
   };
 
@@ -74,6 +108,25 @@ export default function RegisterUpdateForm(props) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSendOTP = async (email) => {
+    const toastId = toast.loading("Loading...");
+    try {
+      setLoading(true);
+      setSendOtpLoading(true);
+      const response = await sendOTP({ email });
+      console.log("Response of OTP sent: ", response);
+      toast.success("OTP sent");
+    } catch (error) {
+      toast.error("OTP not sent");
+      console.log("Error while sending OTP: ", error);
+    } finally {
+      toast.dismiss(toastId);
+      setLoading(false);
+      setSendOtpLoading(false);
+      setSendOtpBtnMessage("OTP Sent");
+    }
   };
 
   return (
@@ -125,41 +178,48 @@ export default function RegisterUpdateForm(props) {
           </div>
         </div>
 
-        {/* Section: Location and Occupation */}
+        {/* Section: Gender and Role */}
         <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+          {/* Gender Select */}
           <div className="flex flex-col w-full md:w-[48%]">
             <label className="relative top-2 w-fit px-1 bg-[#1A1A1A] text-sm left-3 text-[#06B6D4]">
-              Location
+              Gender
             </label>
-            <input
-              type="text"
-              name="location"
-              value={formData.location}
+            <select
+              name="gender"
+              value={formData.gender || "male"}
               onChange={handleInputChange}
-              placeholder="Enter your location"
               className="py-2 px-4 bg-[#1A1A1A] text-white border border-gray-500 rounded focus:outline-none focus:border-2 focus:border-[#06B6D4]"
-            />
-            {formErrors.location && (
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+            {formErrors.gender && (
               <p className="text-red-500 text-xs mt-1 pl-1">
-                {formErrors.location} <sup>*</sup>
+                {formErrors.gender} <sup>*</sup>
               </p>
             )}
           </div>
+
+          {/* Role Select */}
           <div className="flex flex-col w-full md:w-[48%]">
             <label className="relative top-2 w-fit px-1 bg-[#1A1A1A] text-sm left-3 text-[#06B6D4]">
-              Occupation
+              Role
             </label>
-            <input
-              type="text"
-              name="occupation"
-              value={formData.occupation}
+            <select
+              name="role"
+              value={formData.role || "student"}
               onChange={handleInputChange}
-              placeholder="Enter your occupation"
               className="py-2 px-4 bg-[#1A1A1A] text-white border border-gray-500 rounded focus:outline-none focus:border-2 focus:border-[#06B6D4]"
-            />
-            {formErrors.occupation && (
+            >
+              <option value="student">Student</option>
+              <option value="teacher">Teacher</option>
+              <option value="other">Other</option>
+            </select>
+            {formErrors.role && (
               <p className="text-red-500 text-xs mt-1 pl-1">
-                {formErrors.occupation} <sup>*</sup>
+                {formErrors.role} <sup>*</sup>
               </p>
             )}
           </div>
@@ -173,30 +233,11 @@ export default function RegisterUpdateForm(props) {
           <div className="border rounded-md flex items-center justify-center border-gray-500 ">
             <div className="m-4 w-full rounded-md border-2 border-dashed border-[#00D5FA] text-center text-gray-400">
               <ImageUploader
+                setPostImage={setProfilePic}
                 thumbnailLink={props?.isUpdateForm ? props.profilePic : null}
               />
             </div>
           </div>
-        </div>
-
-        {/* Section: Email */}
-        <div className="flex flex-col">
-          <label className="relative top-2 w-fit px-1 bg-[#1A1A1A] text-sm left-3 text-[#06B6D4]">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder="Enter your email"
-            className="py-2 px-4 bg-[#1A1A1A] text-white border border-gray-500 rounded focus:outline-none focus:border-2 focus:border-[#06B6D4]"
-          />
-          {formErrors.email && (
-            <p className="text-red-500 text-xs mt-1 pl-1">
-              {formErrors.email} <sup>*</sup>
-            </p>
-          )}
         </div>
 
         {/* Section: Password */}
@@ -276,6 +317,37 @@ export default function RegisterUpdateForm(props) {
           </Link>
         )}
 
+        {/* Section: Email */}
+        <div className="flex flex-col">
+          <label className="relative  z-20 top-2 w-fit px-1 bg-[#1A1A1A] text-sm left-3 text-[#06B6D4]">
+            Email
+          </label>
+          <div className="relative">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              placeholder="Enter your email"
+              className="py-2 px-4 w-full bg-[#1A1A1A] text-white border border-gray-500 rounded focus:outline-none focus:border-2 focus:border-[#06B6D4]"
+            />
+            <button
+              onClick={() => handleSendOTP(formData.email)}
+              disabled={sendOtpLoading}
+              className={`${
+                sendOtpLoading ? "cursor-not-allowed" : ""
+              } absolute right-2 top-[12%] bg-[#06B6D4] text-black font-semibold py-1 px-3 rounded  hover:bg-[#0284c7] transition-all`}
+            >
+              {sendOtpBtnMessage}
+            </button>
+          </div>
+          {formErrors.email && (
+            <p className="text-red-500 text-xs mt-1 pl-1">
+              {formErrors.email} <sup>*</sup>
+            </p>
+          )}
+        </div>
+
         {/* Section: OTP */}
         {!isUpdateForm && (
           <div className="flex flex-col">
@@ -283,13 +355,13 @@ export default function RegisterUpdateForm(props) {
               OTP
             </label>
             <input
-              type="password"
-              name="confirmPassword"
+              type="number"
+              name="otp"
               value={formData.otp}
               onChange={handleInputChange}
               className="py-2 px-4 bg-[#1A1A1A] text-white border border-gray-500 rounded focus:outline-none focus:border-2 focus:border-[#06B6D4]"
             />
-            {formErrors.confirmPassword && (
+            {formErrors.otp && (
               <p className="text-red-500  text-xs mt-1 pl-1">
                 {formErrors.otp} <sup>*</sup>
               </p>
@@ -303,15 +375,18 @@ export default function RegisterUpdateForm(props) {
         {/* Submit Button */}
         <button
           type="submit"
-          className="bg-[#06B6D4] text-black font-bold py-2 px-4 rounded mt-4 hover:bg-[#0284c7] transition-all"
+          disabled={sendOtpLoading || loading}
+          className={`${
+            loading ? "cursor-not-allowed" : ""
+          } bg-[#06B6D4] text-black font-bold py-2 px-4 rounded mt-4 hover:bg-[#0284c7] transition-all`}
         >
-          {isUpdateForm ? "Update Profile" : "Verify Email / Send OTP"}
+          {isUpdateForm ? "Update Profile" : "Register"}
         </button>
 
         {/* Section: Already have A/C */}
         {!isUpdateForm && (
           <Link
-            to={"/login"}
+            to={"/auth/login"}
             className="text-[#06B6D4] text-left pl-2 font-bold underline italic"
           >
             Already have an account? Login here.
