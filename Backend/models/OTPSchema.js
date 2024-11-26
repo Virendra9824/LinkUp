51d58fd15be7644596b681e8e224f88fde0fe411
Virendra Kumar <vk98982424@gmail.com>
2024-11-26 15:11:10 +0530

    OTP expiration time updated

--- Backend/models/OTPSchema.js
+++ Backend/models/OTPSchema.js
@@ -2,40 +2,35 @@ const mongoose = require("mongoose");
 const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required: true,
-    },
-    otp: {
-        type:Number,
-        required:true,
-    },
-    createdAt: {
-        type:Date,
-        default:Date.now(),
-        expires: 5*60*60,
-    }
+	email: {
+		type: String,
+		required: true,
+	},
+	otp: {
+		type: Number,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+		expires: 120,
+	},
 });
-
 
 //a function -> to send emails
 async function sendVerificationEmail(email, otp) {
-    try{
-        const mailResponse = await mailSender(email,otp);
-        console.log("Email sent Successfully: ", mailResponse);
-    }
-    catch(error) {
-        console.log("error accured while sending mails: ", error);
-        throw error;
-    }
+	try {
+		const mailResponse = await mailSender(email, otp);
+		console.log("Email sent Successfully: ", mailResponse);
+	} catch (error) {
+		console.log("error accured while sending mails: ", error);
+		throw error;
+	}
 }
 
-OTPSchema.pre("save", async function(next) {
-    await sendVerificationEmail(this.email, this.otp);
-    next();
-}) 
-
-
+OTPSchema.pre("save", async function (next) {
+	await sendVerificationEmail(this.email, this.otp);
+	next();
+});
 
 module.exports = mongoose.model("OTP", OTPSchema);
-
