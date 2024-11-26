9e638075e103f19cf3bbb8b4706c15b1c7278281
Virendra Kumar <vk98982424@gmail.com>
2024-11-26 15:13:00 +0530

    user endpoints added

--- Frontend/src/apis/apisEndpoints.js
+++ Frontend/src/apis/apisEndpoints.js
@@ -21,3 +21,8 @@ export const postEndpoints = {
   UPDATE_POST_API_URL: BASE_URL + "/post/update/:id",
   GET_ALL_POSTS_API_URL: BASE_URL + "/post/all",
 };
+
+// USER ENDPOINTS
+export const userEndpoints = {
+  GET_FOLLOWINGS_API_URL: BASE_URL + "/user/followings",
+};
