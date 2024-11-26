9360cb44e8050c2f353c1e4acc8f0af241414dac
Virendra Kumar <vk98982424@gmail.com>
2024-11-26 15:11:48 +0530

    Logout route updated

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const {  login, requestPasswordResetOtp, resetPassword, sendOTP, signUp, logoutUser, deleteAccount } = require("../controllers/authController.js");
+const {
+	login,
+	requestPasswordResetOtp,
+	resetPassword,
+	sendOTP,
+	signUp,
+	logoutUser,
+	deleteAccount,
+} = require("../controllers/authController.js");
 const uploadFile = require("../middlewares/multer");
 const { isAuth } = require("../middlewares/isAuth");
 
 // Public Routes
 router.post("/send-otp", sendOTP);
-router.post("/signup",uploadFile , signUp);
+router.post("/signup", uploadFile, signUp);
 router.post("/login", login);
 
-router.post("/logout", isAuth, logoutUser);
-router.delete("/delete-account",isAuth, deleteAccount );
-
+router.get("/logout", isAuth, logoutUser);
+router.delete("/delete-account", isAuth, deleteAccount);
 
 router.post("/reset-password-otp", requestPasswordResetOtp);
 router.post("/reset-password", resetPassword);
-
-  
-
 
 module.exports = router;
