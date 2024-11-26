2388a6c6821d2cd65f25201d4d335577d6d3852c
Virendra Kumar <vk98982424@gmail.com>
2024-11-26 15:11:32 +0530

    Role field added such as teacher, student or other

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -1,60 +1,61 @@
-
-
-
-
 const { default: mongoose } = require("mongoose");
 
 const userSchema = mongoose.Schema(
-  {
-    firstName: {
-        type:String,
-        required:true,
-        trim:true,
-    },
-    lastName :{
-        type:String,
-        required:true,
-        trim:true,
-    },
-    
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-      enum: ["male", "female", "other"],
-    },
-    followers: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    followings: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    posts: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post",
-      },
-    ],
-    profilePic: {
-      id: String,
-      url: String,
-    },
-  },
-  { timestamps: true }
+	{
+		firstName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		gender: {
+			type: String,
+			required: true,
+			enum: ["male", "female", "other"],
+		},
+		role: {
+			type: String,
+			required: true,
+			enum: ["student", "teacher", "other"],
+		},
+		followers: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		followings: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "User",
+			},
+		],
+		posts: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Post",
+			},
+		],
+		profilePic: {
+			id: String,
+			url: String,
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
