ab5e82001f15b60281d25898ee7c9f7e2015fd92
Virendra Kumar <vk98982424@gmail.com>
2024-11-26 15:12:45 +0530

    Register form updated and FirstView component is removed.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,32 +10,30 @@ import Login from "./pages/Login";
 import DeleteAccount from "./components/forms/DeleteAccount";
 import UpdatePassword from "./components/forms/UpdatePassword";
 import Notification from "./pages/Notification";
-import FirstView from "./pages/firstView";
 import RegisterUpdateForm from "./components/forms/RegisterUpdateForm";
-import SignUpForm from "./components/forms/Signup";
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
-    <div className="w-screen  min-h-screen bg-[#0A0A0A] text-white ">
+    <div className="w-screen  min-h-screen bg-[#0A0A0A] text-white  ">
       <Header />
-      <div className="pt-20">
+      <div className="pt-20 ">
         <Routes>
           <Route path={"/"} element={<Home />} />
-          <Route path={"/new-user"} element={<FirstView />} />
 
-          
           <Route path={"/chat"} element={<Chat />} />
           <Route path={"/notification"} element={<Notification />} />
           <Route path={"/update-profile"} element={<UpdateProfile />} />
-          <Route path={"/auth/signup"} element={<SignUpForm />} />
           <Route path={"/auth/update-password"} element={<UpdatePassword />} />
           <Route path={"/auth/delete-account"} element={<DeleteAccount />} />
-          <Route path={"/auth/register"} element={< RegisterUpdateForm/>} />
+          <Route path={"/auth/register"} element={<RegisterUpdateForm />} />
           <Route path={"/auth/login"} element={<Login />} />
+          <Route path={"/auth/signup"} element={<RegisterUpdateForm />} />
         </Routes>
       </div>
 
       <Footer />
+      <Toaster />
     </div>
   );
 }
