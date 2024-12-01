3ce78fb8c3bfbbc01c2f5564fdfd0e1ff68a0dba
Virendra Kumar <vk98982424@gmail.com>
2024-12-01 18:22:06 +0530

    Userdetails page route is added and chat route is updated.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,6 +12,10 @@ import UpdatePassword from "./components/forms/UpdatePassword";
 import Notification from "./pages/Notification";
 import RegisterUpdateForm from "./components/forms/RegisterUpdateForm";
 import { Toaster } from "react-hot-toast";
+import UserDetails from "./pages/UserDetails";
+import ShowPost from "./components/posts/ShowPost";
+import UserSpecificPosts from "./components/posts/UserSpecificPosts";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
@@ -21,7 +25,7 @@ export default function App() {
         <Routes>
           <Route path={"/"} element={<Home />} />
 
-          <Route path={"/chat"} element={<Chat />} />
+          <Route path={"/chat/:receiverId"} element={<Chat />} />
           <Route path={"/notification"} element={<Notification />} />
           <Route path={"/update-profile"} element={<UpdateProfile />} />
           <Route path={"/auth/update-password"} element={<UpdatePassword />} />
@@ -29,6 +33,12 @@ export default function App() {
           <Route path={"/auth/register"} element={<RegisterUpdateForm />} />
           <Route path={"/auth/login"} element={<Login />} />
           <Route path={"/auth/signup"} element={<RegisterUpdateForm />} />
+          <Route path={"/user/:userId"} element={<UserDetails />} />
+          <Route
+            path={"/user/:userId/posts/:postId"}
+            element={<UserSpecificPosts />}
+          />
+          <Route path={"*"} element={<Error />} />
         </Routes>
       </div>
 
