32912fc26bef2190e8241f33362761f135d7be4f
Virendra Kumar <vk98982424@gmail.com>
2024-12-01 18:22:43 +0530

    Wrapped the component to enable react-router functionality.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { rootReducer } from "./redux/reducers/reducers.js";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { BrowserRouter } from "react-router-dom";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
