ea41c856bf7aff629f3a567f3a08a9e2a54dae88
Virendra Kumar <vk98982424@gmail.com>
2024-12-01 18:26:44 +0530

    Comment card is updated to get the userId and postId for deletion of comment.

--- Frontend/src/components/posts/CommentCard.jsx
+++ Frontend/src/components/posts/CommentCard.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
 import { MdOutlineDelete } from "react-icons/md";
 import { deleteComment } from "../../apis/postApi";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 export default function CommentCard(props) {
-  let { firstName, lastName, commentData, profilePic, postId, commentId } =
-    props;
+  let {
+    firstName,
+    lastName,
+    commentData,
+    profilePic,
+    postId,
+    commentId,
+    userId,
+    currentUserId,
+    postOwnerId,
+  } = props;
   const [deleteCommentLoading, setDeleteCommentLoading] = useState(false);
 
   let handleDeleteComment = async (postId) => {
@@ -12,9 +23,11 @@ export default function CommentCard(props) {
       setDeleteCommentLoading(true);
       const response = await deleteComment({ postId, commentId });
       console.log(response.message);
+      toast.success(response.message);
       // Refresh the comments after deletion
     } catch (error) {
       console.error("Error deleting comment:", error);
+      toast.error("Error in deleting comment.");
     } finally {
       setDeleteCommentLoading(false);
     }
@@ -29,15 +42,19 @@ export default function CommentCard(props) {
           className="w-8 h-8 rounded-full object-cover"
         />
         <div className="flex flex-col">
-          <span className="text-gray-200 font-semibold">
+          <Link to={`/user/${userId}`} className="text-gray-200 font-semibold">
             {firstName} {lastName}
-          </span>
+          </Link>
           <span className="text-gray-400 text-sm">{commentData}</span>
         </div>
         <button
           onClick={() => handleDeleteComment(postId)}
           disabled={deleteCommentLoading}
-          className="ml-auto text-gray-100 hover:text-red-500"
+          className={`${
+            userId !== currentUserId && currentUserId !== postOwnerId
+              ? "hidden"
+              : ""
+          } ml-auto text-gray-100 hover:text-red-500`}
         >
           <MdOutlineDelete size={22} />
         </button>
