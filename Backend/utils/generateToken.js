1403e0d26fa4fcf6a9172f8902c39bc2a97454ee
Virendra Kumar <vk98982424@gmail.com>
2024-12-01 18:19:51 +0530

    Added return statement for token.

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,14 +1,18 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id, res) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "15d" });
+	const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: "15d",
+	});
 
-  res.cookie("token", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: "strict",
-    // secure: true,
-  });
+	res.cookie("token", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000,
+		httpOnly: true,
+		sameSite: "strict",
+		// secure: true,
+	});
+
+	return token;
 };
 
 module.exports = generateToken;
